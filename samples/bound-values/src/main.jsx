0c8a920c7a64efa98de5b2fadc6a13b775c27174
Hawk Ticehurst <hawkticehurst@gmail.com>
2025-12-13 16:45:53 -0500

    Add sample

---
+++ samples/bound-values/src/main.jsx
@@ -0,0 +1,20 @@
+import { defineComponent, cell, get, set } from 'rift-js';
+import './styles.css';
+
+function BoundValues() {
+	const count = cell(0);
+
+	const increment = () => set(count, get(count) + 1);
+	const setCount = (e) => set(count, parseInt(e.target.value, 10));
+
+	return (
+		<>
+			<button onclick={increment}>Clicked {get(count)} times</button>
+			<button onclick={increment}>Clicked {get(count)} times</button>
+			<input type="number" value={get(count)} oninput={setCount} />
+			<p>Bound to count state: {get(count)}</p>
+		</>
+	);
+}
+
+defineComponent('bound-values', BoundValues);
