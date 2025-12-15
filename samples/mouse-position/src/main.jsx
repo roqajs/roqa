a4e1709835417687a4d2002610a166e63ba6a9ef
Hawk Ticehurst <hawkticehurst@gmail.com>
2025-12-14 21:15:33 -0500

    Add new sample

---
+++ samples/mouse-position/src/main.jsx
@@ -0,0 +1,26 @@
+import { defineComponent, cell, get, set } from 'rift-js';
+import './styles.css';
+
+function MousePosition() {
+	const x = cell(0);
+	const y = cell(0);
+	let clientRect;
+	
+	this.connected(() => {
+		clientRect = this.getBoundingClientRect();
+		this.on('mousemove', updatePosition);
+	});
+	
+	const updatePosition = (event) => {
+		set(x, event.clientX - Number(clientRect.left.toFixed(0)));
+		set(y, event.clientY - Number(clientRect.top.toFixed(0)));
+	};
+
+	return (
+		<p>
+			The mouse position is {get(x)} x {get(y)}
+		</p>
+	);
+}
+
+defineComponent('mouse-position', MousePosition);
