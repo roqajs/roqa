628ffd054c67c96df246238b33c9f4a902f03587
Hawk Ticehurst <hawkticehurst@gmail.com>
2026-01-10 04:06:53 -0500

    Add compiler API types

---
+++ packages/roqa/src/compiler/index.d.ts
@@ -0,0 +1,5 @@
+export function compile(code: string, filename: string): { code: string; map: any };
+export function parse(code: string, filename: string): any;
+export function validateNoCustomComponents(ast: any): void;
+export function generateOutput(code: string, ast: any, filename: string): { code: string; map: any };
+export function inlineGetCalls(code: string, filename: string): { code: string; map: any };
